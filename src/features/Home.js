f7473eebe58771c25118d8d38112351f04fcab5b
Civilight <pattarapon_ja@cmu.ac.th>
2025-10-11 15:55:48 +0700

    11oct

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../features/Product/index";
 import AddForm from "../features/Product/AddForm";
-import data from "../app/data";
+// import data from "../app/data";
+import axios from 'axios';
 
 let currentProductId = 9;
 
 function Home() {
-  //const [products, setProducts] = useState([]);
-  const [products, setProducts] = useState(data);
+  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState(data);
+useEffect(() => {
+  async function getProducts() {
+    const products = await axios.get(
+      'https://68e9fc47f1eeb3f856e5a63c.mockapi.io/products'
+    );
+    setProducts(products.data);
+  }
+
+  getProducts();
+}, []);
 
   const addProduct = (product) => {
     const newProduct = { id: ++currentProductId, ...product };
